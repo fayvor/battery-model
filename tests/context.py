901b9599c2b2166bbac6d6fbfbf63538c1c919c3
Fayvor Love <fayvor@gmail.com>
2021-02-18 19:17:18 -0500

    Add comments to context.py.

--- tests/context.py
+++ tests/context.py
@@ -1,3 +1,7 @@
+"""
+Context for tests. Modifies sys.path to add relative path to the main module.
+See: https://docs.python-guide.org/writing/structure/
+"""
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
